7d649f222a204dcc00b16295cac3c0e968a05dea
Nikola <nikola94lukic@hotmail.com>
2021-05-29 12:47:20 +0200

    Add project files.

---
+++ PizzaPlace/Models/IPizzaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Models
+{
+    public interface IPizzaRepository
+    {
+        IEnumerable<Pizza> AllPizzas { get; }
+
+        IEnumerable<Pizza> PizzaOfTheWeek { get; }
+
+        Pizza GetPizzaById(int PizzaId);
+    }
+}
